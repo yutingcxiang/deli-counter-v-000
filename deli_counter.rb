9b8f803487799be42da85f5bd61c076f48a0dee3
Christine Xiang <yutingcxiang@gmail.com>
2018-07-06 22:07:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,6 +9,7 @@ def take_a_number(katz_deli, name)
   katz_deli << name
 end
 
-def now_serving
-  
+def now_serving(katz_deli)
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.unshift()
 end
